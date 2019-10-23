2dc4a5c64a052cbd103a38964d21eed78fb70f05
Rodrigo Faustino <ev.rodrigo.eterno@gmail.com>
2019-10-22 21:57:41 -0300

    fff

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:lista_de_compras/telas/edita_compra.dart';
 import 'package:lista_de_compras/telas/lista_de_compras.dart';
 
 void main() => runApp(MaterialApp(
-  home: EditaCompra(titulo:'Compras'),
+  home: ListaDeCompras(),
   theme: ThemeData(
+    //vvvv
     primaryColor: Colors.amberAccent,
     primarySwatch: Colors.amber,
     accentColor: Colors.yellow[100],
